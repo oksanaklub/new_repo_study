4e094218c6628993ccf6b0cb5402c13436c2c7f5
Oksana Klub <oksananov22@gmail.com>
2023-10-08 23:13:46 +0300

    HW 6 is completed. Added folder with 3 files inside.

--- HomeWork6/2nd.py
+++ HomeWork6/2nd.py
@@ -1,7 +1,7 @@
-str = "hello 1 one two three 15 world"
-words = str.split()
+text = "hello 1 one two three 15 world"
+words = text.split()
 
 for i in range(len(words) - 2):
     if words[i].isalpha() and words[i + 1].isalpha() and words[i + 2].isalpha():
         break
-print(f"This '{str}'  has 3 words in line")
+print(f"This '{text}'  has 3 words in line")
