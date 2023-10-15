ea6e8785832dbf9edcd639898044eee4af8aa98d
Oksana Klub <oksananov22@gmail.com>
2023-10-15 17:47:43 +0300

    HW 7 is completed. Added folder with 4 files inside. Task number 4 is partly completed. Need more time to think about it.

---
+++ HomeWork7/1st.py
@@ -0,0 +1,26 @@
+import os
+
+path = "/home"
+
+# print(os.getcwd())
+# os.chdir("/Users/oksanaklub/PycharmProjects/pythonProject/new_repo_study")
+
+# os.walk(".")
+# print(os.path.join(path, "User/Desktop", "file.txt"))
+
+# print(os.mkdir("Test1"))
+# print(os.makedirs("Test2"))
+# print(os.removedirs("Test1"))
+# print(os.rmdir("Test2"))
+# os.makedirs("Test3")
+# os.rename("Test3", "New")
+
+# os.getcwd()
+# print(os.stat("1st.py"))
+
+# print(os.environ.get("PATH"))
+
+# file_path = "/path/to/some/file.txt"
+# directory, filename = os.path.split(file_path)
+# print("Directory:", directory)
+# print("Filename:", filename)
---
+++ HomeWork7/4th.py
@@ -0,0 +1,15 @@
+# Задача 4:
+# Написати програму, яка на вхід приймає список чисел і перевіряє, чи всі числа в цій послідовності є унікальними
+numbers = input().split()
+import pdb
+pdb.set_trace()
+if numbers[0] != numbers[1] != numbers[2]:
+    print("Unique")
+else:
+    print("Not unique")
+
+
+# if len(numbers) == len(set(numbers)):
+#     continue
+
+# Couldn't solve the task yet. Need more time to think about it.
